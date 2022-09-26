b25e59ed8c4f411577fd8c2c60d50ec732b79eb1
Jawneee <jawnabellera25@gmail.com>
2022-09-26 10:52:29 -0500

    Commit waaaasdacw

---
+++ helloGit/src/helloGit/HiGit.java
@@ -0,0 +1,11 @@
+package helloGit;
+
+public class HiGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, git!");
+		System.out.println("WASa");
+		System.out.println("ASD");
+	}
+
+}
